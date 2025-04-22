d0ad269f9e85cfb34c162e0b824eb0c0ff2e7daa
SakshiLadole06 <ladolesakshi@gmail.com>
2025-04-22 18:49:18 +0530

    Completed first day assignment dated 14/04/25
    In this assignment total 4 task is included
    1. Hello Flutter
    2. Personal Info
    3. Row App
    4. Column App

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,69 @@
+import 'package:c2w_assignment_task/day01/FirstTaskPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: TaskApp(),
+    );
+  }
+}
+
+class TaskApp extends StatefulWidget{
+  const TaskApp({super.key});
+
+  @override
+  State<TaskApp> createState() => _TaskAppState();
+}
+
+class _TaskAppState extends State<TaskApp>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Assignment Task",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
         ),
+        centerTitle: true,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(12),
+          child : SingleChildScrollView(
+            child : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 330,
+                  child : ElevatedButton(
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FirstTaskPage()),
+                      );
+                    },
+                    child: Text(
+                      "Day 01(14/04/2025)",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
       ),
     );
   }
